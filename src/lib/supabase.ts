1ff0b88059951299fe0df878e6c60099de0709eb
Sharbel Habchy <sharbelhabchy@gmail.com>
2025-10-18 13:45:13 -0500

    login

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
+import {Database} from "@/types/supabase.ts";
 
-const supabaseUrl = 'https://hfagyeengtksiadlbroim.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhZ3llZW5ndGtzaWFkbGJyb2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NDE3NzIsImV4cCI6MjA3NjIxNzc3Mn0.22bAyHr9tlyXzNE1eXpVYaMynhthNaYbGZ-eQBWrkrI'; // Replace with your anon key
+const SUPABASE_URL = 'https://fagyeengtksiadlbroim.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhZ3llZW5ndGtzaWFkbGJyb2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NDE3NzIsImV4cCI6MjA3NjIxNzc3Mn0.22bAyHr9tlyXzNE1eXpVYaMynhthNaYbGZ-eQBWrkrI'; // Replace with your anon key
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+    auth: {
+        storage: localStorage,
+        persistSession: true,
+        autoRefreshToken: true,
+    }
+});
